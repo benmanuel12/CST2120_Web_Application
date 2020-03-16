d44dfe4e6ff641322ba971b7f5b434c442aff8b5
Benjamin Manuel <contact.ben.manuel@gmail.com>
2020-03-16 20:50:55 +0000

    added the start of sql stuff

--- index.js
+++ index.js
@@ -16,19 +16,81 @@ const connectionPool = mysql.createPool({
 
 const app = express();
 
-function GETupdate(request, response) {
+async function findUserID() {
     //Build query
-    let sql = "SELECT multiverseID FROM ownedcards";
+    let sql = "SELECT id FROM users WHERE active = 'Yes';";
+    return new Promise((resolve, reject) => {
+        connectionPool.query(sql, (err, result) => {
+            if (err) { //Check for errors
+                reject("Error executing query: " + JSON.stringify(err));
+            } else { //Output results in JSON format - a web service would return this string.
+                resolve(result);
+            }
+        });
+    })
+}
 
-    //Execute query and output results
-    connectionPool.query(sql, (err, result) => {
-        if (err) { //Check for errors
-            console.error("Error executing query: " + JSON.stringify(err));
-        } else { //Output results in JSON format - a web service would return this string.
-            response.send(JSON.stringify(result));
+async function findCardIDs(userid) {
+    //Build query
+    let sql = "SELECT multiverseID FROM ownedCards WHERE ownedBy = " + userid + ";";
+
+    return new Promise((resolve, reject) => {
+        connectionPool.query(sql, (err, result) => {
+            if (err) {
+                reject("Error executing query: " + JSON.stringify(err));
+            } else {
+                resolve(result);
+            }
+        });
+    })
+}
+
+async function getCardData(ids) {
+    //Build query
+    let sql = "SELECT * FROM cards WHERE ";
+    for (let i = 0; i < ids.length; i++) {
+        sql = sql + "multiverseID = " + ids[i];
+        if (i + 1 != ids.length) {
+            sql = sql + " OR ";
+        } else {
+            sql = sql + ";";
         }
-    });
+    }
+    return new Promise((resolve, reject) => {
+        connectionPool.query(sql, (err, result) => {
+            if (err) { //Check for errors
+                reject("Error executing query: " + JSON.stringify(err));
+            } else {
+                resolve(result);
+            }
+        });
+    })
+}
 
+function GETupdate(request, response) {
+    findUserID().then(result => {
+        idObject = JSON.parse(result);
+        id = idObject.id;
+
+        findCardIDs(id).then(result => {
+            cardIDJSON = JSON.parse(result);
+            let cardIDs = [];
+            for (let i = 0; i < cardIDJSON.length; i++) {
+                cardIDs.push(i.multiverseID);
+            }
+            getCardData(cardIDs).then(result => {
+                response.send(result);
+
+            }).catch(err => {
+                console.error(JSON.stringify(err));
+            })
+
+        }).catch(err => {
+            console.error(JSON.stringify(err));
+        })
+    }).catch(err => {
+        console.error(JSON.stringify(err));
+    })
 }
 
 app.get("/update", GETupdate);
--- public/update.js
+++ public/update.js
@@ -4,7 +4,48 @@ function update() {
     request.onload = function() {
         if (request.status === 200) {
             let responseData = request.responseText;
-            console.log(JSON.parse(responseData));
+            let objectArray = JSON.parse(responseData);
+            let largerHTMLString = "";
+            for (let i = 0; i < objectArray.length; i++) {
+
+                let name = i.name;
+                let manaCost = i.manaCost;
+                let cmc = i.cmc;
+                let colors = i.colors;
+                let colorIdentity = i.colorIdentity;
+                let type = i.type;
+                let supertypes = i.supertypes;
+                let types = i.types;
+                let subtypes = i.subtypes;
+                let rarity = i.rarity;
+                let set = i.set;
+                let setname = i.setname;
+                let text = i.text;
+                let flavor = i.flavor;
+                let artist = i.artist;
+                let number = i.number;
+                let power = i.power;
+                let toughness = i.toughness;
+                let loyalty = i.loyalty;
+                let layout = i.layout;
+                let multiverseID = i.multiverseID;
+                let imageUrl = i.imageUrl;
+                let printings = i.printings;
+                let legalities = i.legalities;
+                let id = i.id;
+
+                let dataArray = { name, manaCost, cmc, colors, colorIdentity, type, supertypes, types, subtypes, rarity, set, setname, text, flavor, artist, number, power, toughness, loyalty, layout, multiverseID, imageUrl, printings, legalities, id };
+
+                let htmlString = "<tr>";
+                for (let j = 0; j < dataArray.length; j++) {
+                    let newString = "<td>" + dataArray[j] + "</td>";
+                    htmlString += newString;
+                }
+                htmlString += "</tr>";
+                largerHTMLString += htmlString;
+            }
+            document.getElementById("rowstart").innerHTML = largerHTMLString;
+
         } else
             alert("error: " + request.status);
 
@@ -20,4 +61,5 @@ for each row in the results from SELECT multiverseID from ownedcards WHERE usern
 
 SELECT * FROM cards where multiverseID = any value in array
 For each item in results, convert to JS object and insert into table
-*/
+
+also need to get quantity from owned card table somehow
